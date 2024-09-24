b92937d6b741b51f090c038beb7c399fe188da89
will chandler <jaskamiin@gmail.com>
2024-09-23 21:54:40 -0400

    disable lf queue for now, unsafe

--- ismillion.js
+++ ismillion.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const { Worker } = require('worker_threads');
 const { createLRU } = require('lru.min');
-
 const config = require('./config.json');
 
 const FILE_PATH = path.resolve(__dirname, config.filePath);
@@ -11,8 +10,35 @@ const CHUNK_BYTE_SIZE = config.chunkByteSize;
 
 const chunkCache = createLRU({ max: 50 }); 
 
-let index;
+//TODO: uncomment to enable  if a safer method found
+//const LFQueue = require('./lib/lfqueue');
+//const task_queue = new LFQueue(100);
+//const result_queue = new LFQueue(100);
+// function startWorkers(numWorkers) {
+//     for (let i = 0; i < numWorkers; i++) {
+// 	const worker = new Worker(path.resolve(__dirname, 'chunkWorker.js'));
+// 	worker.on('message', (result) => {
+// 	    resultQueue.enqueue(result);
+// 	});
+// 	worker.on('error', (error) => {
+// 	    console.error('Worker error:', error);
+// 	});
+//     }
+// }
+// async function processChunks() {
+//     while (true) {
+// 	const task = taskQueue.dequeue();
+// 	if (task === null) {
+// 	    // no busy waiting
+// 	    await new Promise(resolve => setTimeout(resolve, 10)); 
+// 	    continue;
+// 	}
+// 	// TODO: process task here
+//     }
+// }
 
+
+let index;
 async function initialize() {
   if (!fs.existsSync(INDEX_PATH)) {
     const generateIndex = require('./lib/generateIndex.js');
@@ -22,22 +48,23 @@ async function initialize() {
   index = JSON.parse(fs.readFileSync(INDEX_PATH, 'utf8'));
 }
 
+
 function findChunkForNumber(n) {
-  for (const chunkInfo of index) {
-    if (n >= chunkInfo.startNum && n <= chunkInfo.endNum) {
-      return chunkInfo;
+    for (const chunkInfo of index) {
+	if (n >= chunkInfo.startNum && n <= chunkInfo.endNum) {
+	    return chunkInfo;
+	}
     }
-  }
-  return null;
+    return null;
 }
 
 function readChunk(chunkInfo) {
     const cacheKey = `${chunkInfo.startNum}-${chunkInfo.endPos}`;
-
+    
     if (chunkCache.has(cacheKey)) {
         return Promise.resolve(chunkCache.get(cacheKey));
     }
-
+    
     return new Promise((resolve, reject) => {
         const isLastChunk = chunkInfo === index[index.length - 1];
         const worker = new Worker(path.resolve(__dirname, 'lib/chunkWorker.js'), {
